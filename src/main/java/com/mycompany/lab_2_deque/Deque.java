d8d09b859d6bd5025330b3c1b93c102c4d96f523
Yaroslav Trykoz <trikozaroslav@gmail.com>
2020-09-27 20:23:51 +0300

    Deque constructor

--- src/main/java/com/mycompany/lab_2_deque/Deque.java
+++ src/main/java/com/mycompany/lab_2_deque/Deque.java
@@ -10,5 +10,17 @@ package com.mycompany.lab_2_deque;
  * @author yaroslavtrykoz
  */
 public class Deque {
+    static final int MAX = 10; 
+    int  arr[]; 
+    int  front; 
+    int  rear; 
+    int  size;
     
+    public Deque(int size) 
+    { 
+        arr = new int[MAX]; 
+        front = -1; 
+        rear = 0; 
+        this.size = size; 
+    }
 }
