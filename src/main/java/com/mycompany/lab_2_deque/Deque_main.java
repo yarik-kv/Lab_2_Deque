9cf5c60aa0c28d3aebef705a242148e08f1cef0d
Yaroslav Trykoz <trikozaroslav@gmail.com>
2020-09-27 20:41:09 +0300

    Deque_main modified

--- src/main/java/com/mycompany/lab_2_deque/Deque_main.java
+++ src/main/java/com/mycompany/lab_2_deque/Deque_main.java
@@ -10,5 +10,32 @@ package com.mycompany.lab_2_deque;
  * @author yaroslavtrykoz
  */
 public class Deque_main {
-    
+    public static void main(String[] args) 
+    { 
+          
+         Deque dq = new Deque(5); 
+           
+         System.out.println("Insert element at rear end  : 5 "); 
+         dq.insertrear(5); 
+           
+         System.out.println("insert element at rear end : 10 "); 
+         dq.insertrear(10); 
+           
+         System.out.println("get rear element : "+ dq.getRear()); 
+           
+         dq.deleterear(); 
+         System.out.println("After delete rear element new rear become : " +  
+                                dq.getRear()); 
+           
+         System.out.println("inserting element at front end"); 
+         dq.insertfront(15); 
+           
+         System.out.println("get front element: " +dq.getFront()); 
+           
+         dq.deletefront(); 
+           
+         System.out.println("After delete front element new front become : " + 
+                                    +  dq.getFront()); 
+          
+    } 
 }
