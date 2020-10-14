34bdbb207f87de8bb809bdb0d70853ebacadd94a
Yaroslav Trykoz <yaroslavtrykoz@MacBook-Pro-Aroslav.local>
2020-10-14 20:33:34 +0300

    pom.xml modified

--- src/test/java/com/mycompany/lab_2_deque/Deque_Test.java
+++ src/test/java/com/mycompany/lab_2_deque/Deque_Test.java
@@ -8,6 +8,8 @@ package com.mycompany.lab_2_deque;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -97,4 +99,5 @@ public class Deque_Test {
         assertEquals(expected,dq.isFull());
         System.out.println("Success!\n");
     }
+    
 }
